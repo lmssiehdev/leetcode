a0daed70e9ff3a9fd4e46e9ad5192252f0531bec
lmssieh <122186255+lmssiehdev@users.noreply.github.com>
2023-12-22 20:07:50 +0100

    Time: 52 ms (79.42%) | Memory: 42.9 MB (46.46%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.ts
+++ 1013-fibonacci-number/fibonacci-number.ts
@@ -1,4 +1,10 @@
+const cache = {}
+
 function fib(n: number): number {
   if (n <= 1) return n;
-  return fib(n - 1) + fib(n - 2)
+
+  if (cache[n] != null) return cache[n]
+
+  cache[n] = fib(n - 1) + fib(n - 2 )
+  return cache[n]
 };
