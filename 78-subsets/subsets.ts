23a7594730a89f9ceea0713b0ea228ee307ebec5
lmssieh <122186255+lmssiehdev@users.noreply.github.com>
2023-12-23 04:13:14 +0100

    Time: 55 ms (80.49%) | Memory: 45.2 MB (50.52%) - LeetSync

--- 78-subsets/subsets.ts
+++ 78-subsets/subsets.ts
@@ -2,23 +2,21 @@
 function subsets(nums: number[]): number[][] {
   const result = []
 
-  function dfs(nums, slate, idx) {
-    // base
+  function backtrack(slate, idx) {
     if ( idx === nums.length) {
-      result.push([...slate]);
+      result.push([...slate])
       return;
     }
-
     // exclude
-    dfs(nums, slate, idx + 1)
+    backtrack(slate, idx + 1);
 
     // include
-    slate.push(nums[idx]);
-    dfs(nums, slate, idx + 1)
-    slate.pop();
+    slate.push(nums[idx])
+    backtrack(slate, idx + 1);
+    slate.pop()
   }
 
-  dfs(nums, [], 0)
+  backtrack([], 0)
 
-  return result
+  return result;
 };
