f2b74ad084a6c1300c07510d6bbf59d7659ea0f2
lmssieh <122186255+lmssiehdev@users.noreply.github.com>
2023-12-23 03:48:52 +0100

    Time: 63 ms (21.89%) | Memory: 44.9 MB (20.90%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.ts
+++ 216-combination-sum-iii/combination-sum-iii.ts
@@ -1,35 +1,32 @@
 function combinationSum3(k: number, n: number): number[][] {
-    const result = [];
+  const result = [];
+  const seen = new Set();
 
-  function backtrack(slate, start, sum) {
+  function backtrack(slate, i, sum) {
     if (sum === n && slate.length === k) {
-      result.push([...slate]);
+      result.push([...slate])
       return;
     }
+  
 
-    for (let i = start; i <= 9; i++) {
-      if ( sum + i > n ) {
-        continue; 
-      }
+    for (let j = i; j <= 9; j++) {
+      
+      if (seen.has(j)) continue;
+      if ( sum + j > n ) continue;
+      
+      seen.add(j)
+      slate.push(j)
 
-      slate.push(i);
-      backtrack(slate, i + 1, sum + i);
+      backtrack(slate, j + 1, sum + j );
+
       slate.pop();
+      seen.delete(j)
+
     }
-  }
 
-    backtrack([], 1, 0)
+  } 
 
-    return result;
-};
+  backtrack([], 1, 0)
 
-
-// use a recursive backtracking appraoch.
-// use a set to keep track of seen numbers.
-// iterate over k and recursively call the fn if
-  // number doesn't exist in the set
-  // count is less than K
-  // add the number to a slate
-  // call
-  // remove it.
-// return;
+  return result;
+}
