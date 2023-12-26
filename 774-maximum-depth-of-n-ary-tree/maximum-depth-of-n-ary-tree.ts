63a52788967260c5eabe06e0ca20f7d0e37773f4
lmssieh <122186255+lmssiehdev@users.noreply.github.com>
2023-12-26 05:46:21 +0100

    Time: 83 ms (11.76%) | Memory: 45.7 MB (85.88%) - LeetSync

---
+++ 774-maximum-depth-of-n-ary-tree/maximum-depth-of-n-ary-tree.ts
@@ -0,0 +1,32 @@
+/**
+ * Definition for Node.
+ * class Node {
+ *     val: number
+ *     children: Node[]
+ *     constructor(val?: number, children?: Node[]) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.children = (children===undefined ? [] : children)
+ *     }
+ * }
+ */
+
+function maxDepth(root: Node | null): number {
+    if ( !root ) return 0;
+    let depth = 0;
+
+    const queue: Node[] = [root];
+
+    while ( queue.length ) {
+      const level = queue.length;
+      
+      for ( let i = 0; i < level; i++ ) {
+        const node = queue.shift();
+        queue.push(...node.children)
+      }
+
+      ++depth;
+    }
+
+
+    return depth;
+};
