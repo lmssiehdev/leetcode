6d5988b2332f9b0521f6eee6eb0f5c67d30a35c8
lmssieh <122186255+lmssiehdev@users.noreply.github.com>
2023-12-29 21:14:32 +0100

    Time: 96 ms (5.53%) | Memory: 43.9 MB (97.67%) - LeetSync

--- 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.ts
+++ 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.ts
@@ -1,8 +1,5 @@
 function kidsWithCandies(candies: number[], extraCandies: number): boolean[] {
   let greatest = Math.max(...candies);
 
-  return candies.map((candiesCount) => {
-    if ( candiesCount + extraCandies >= greatest) return true; 
-    return false;
-  })
-};
+  return candies.map((candiesCount) => (candiesCount + extraCandies >= greatest))
+  }
