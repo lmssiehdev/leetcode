8ae75f99358f0f61f22a5fc345e143ba539ec5cf
lmssieh <122186255+lmssiehdev@users.noreply.github.com>
2023-12-30 21:08:40 +0100

    Time: 154 ms (5.01%) | Memory: 49 MB (99.05%) - LeetSync

--- 344-reverse-string/reverse-string.ts
+++ 344-reverse-string/reverse-string.ts
@@ -2,13 +2,5 @@
  Do not return anything, modify s in-place instead.
  */
 function reverseString(s: string[]): void {
-    let start = 0;
-    let end = s.length - 1;
-
-    while (start <= end) {
-        [s[start], s[end]] = [s[end], s[start]];
-
-        start++;
-        end--;
-    }
+    s.reverse()
 };
