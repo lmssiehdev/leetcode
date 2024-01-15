0570eb0017102d6fd4968ac784f83a6d4ffedad8
lmssieh <122186255+lmssiehdev@users.noreply.github.com>
2024-01-15 05:56:18 +0100

    Time: 80 ms (84.49%) | Memory: 21.2 MB (30.43%) - LeetSync

---
+++ 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.py
@@ -0,0 +1,16 @@
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        self.min_heap = []
+        self.k = k
+
+        for num in nums:
+            self.add(num)
+
+    def add(self, val: int) -> int:
+        heappush(self.min_heap, val)
+
+        if len(self.min_heap) > self.k:
+            heappop(self.min_heap)
+
+        return self.min_heap[0]
