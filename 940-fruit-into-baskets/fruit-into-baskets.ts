f69400601ed530ab2a50bab43983631a38855688
lmssieh <122186255+lmssiehdev@users.noreply.github.com>
2024-01-22 11:29:37 +0100

    Time: 107 ms (53.52%) | Memory: 61 MB (32.39%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.ts
+++ 940-fruit-into-baskets/fruit-into-baskets.ts
@@ -1,26 +1,23 @@
 function totalFruit(fruits: number[]): number {
-    let basket: Record<string, number> = {};
+    let basket = new Map<string | number, number>();
     let maxFruits = Number.MIN_SAFE_INTEGER;
     let windowStart = 0;
 
     for (let windowEnd = 0; windowEnd < fruits.length; windowEnd++) {
         const curr = fruits[windowEnd];
 
-        while (Object.keys(basket).length >= 2 && basket[curr] === undefined) {
-            basket[fruits[windowStart]]--;
-            if (basket[fruits[windowStart]] === 0) {
-                delete basket[fruits[windowStart]];
+        while (basket.size >= 2 && !basket.has(curr)) {
+            const first = fruits[windowStart];
+            basket.set(first, basket.get(first) - 1)
+            if (basket.get(first) === 0) {
+                basket.delete(first);
             }
             windowStart++;
         }
 
-        if (basket[curr] !== undefined) {
-            basket[curr]++;
-        } else {
-            basket[curr] = 1;
-        }
+        basket.set(curr, ( basket.get(curr) ?? 0 ) + 1 )
 
-        maxFruits = Math.max(maxFruits, Object.values(basket).reduce((c, p) => c + p, 0))
+        maxFruits = Math.max(maxFruits, windowEnd - windowStart + 1)
     }
     return maxFruits
 };
