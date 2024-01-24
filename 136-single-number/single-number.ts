3eb7ca496a63dde5e5f5670d10f0396025224bcf
lmssieh <122186255+lmssiehdev@users.noreply.github.com>
2024-01-24 13:04:19 +0100

    Time: 50 ms (97.06%) | Memory: 56.3 MB (5.36%) - LeetSync

---
+++ 136-single-number/single-number.ts
@@ -0,0 +1,8 @@
+function singleNumber(nums: number[]): number {
+   const set = new Set<number>();
+   for ( const num of nums ) {
+     if ( set.has(num) ) set.delete(num)
+     else set.add(num)
+   }
+   return [...set][0] 
+};
