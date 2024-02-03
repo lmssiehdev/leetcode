de3e9c91b9985472904181d02107a8a3fe1df96b
lmssieh <122186255+lmssiehdev@users.noreply.github.com>
2024-02-03 22:04:29 +0100

    Time: 103 ms (22.01%) | Memory: 58.2 MB (5.77%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.js
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.js
@@ -2,6 +2,36 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var sortedSquares = function(nums) {
-    return nums.map(num => num ** 2).sort((a,b) => a-b)
+var sortedSquares = function (nums) {
+  const negative = [];
+  const positive = [];
+
+  for (const num of nums) {
+    let square = num ** 2;
+    if (num < 0) {
+      negative.unshift(square);
+    } else {
+      positive.push(square);
+    }
+  }
+
+  const sorted = [];
+
+  while (negative.length !== 0 || positive.length !== 0) {
+    if (negative.length === 0) {
+      sorted.push(...positive);
+      break;
+    }
+    if (positive.length === 0) {
+      sorted.push(...negative);
+      break;
+    }
+    if (negative[0] < positive[0]) {
+      sorted.push(negative.shift());
+    } else {
+      sorted.push(positive.shift());
+    }
+  }
+
+  return sorted;
 };
