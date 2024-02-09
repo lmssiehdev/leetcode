325762203a38855a00499f537387885661f0d10a
lmssieh <122186255+lmssiehdev@users.noreply.github.com>
2024-02-09 21:35:13 +0100

    Time: 58 ms (54.84%) | Memory: 52.4 MB (9.68%) - LeetSync

--- 2401-count-asterisks/count-asterisks.ts
+++ 2401-count-asterisks/count-asterisks.ts
@@ -1,10 +1,19 @@
 function countAsterisks(s: string): number {
-    const array = s.split("|").filter((s, i) => i % 2 === 0);
-    const shortString = array.join("")
+  let pipes = 0;
+  let asterisks = 0;
+
+  for ( const char of s ) {
+    if ( char === "|" ) ++pipes
+    if ( char === "*" && pipes % 2 === 0 ) ++asterisks
+  }
+
+  return asterisks;
+  // const array = s.split("|").filter((s, i) => i % 2 === 0);
+  // const shortString = array.join("")
 
 
-    return Array.from(shortString).reduce((count, char) => {
-      if ( char == "*" ) ++count;
-      return count;
-    }, 0);
+  // return Array.from(shortString).reduce((count, char) => {
+  //   if ( char == "*" ) ++count;
+  //   return count;
+  // }, 0);
 };
